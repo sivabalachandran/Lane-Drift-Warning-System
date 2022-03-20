bfdffb5d1ee5137c7263be80373053403209fb7a
Siva Balachandran <sivamech@gmail.com>
2022-03-20 19:44:00 -0400

    [Feature] - With longer video and adjusted thresholding.

---
+++ cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf frames/*
+rm -rf detected/*
+rm lane_detected.mp4
--- lane_detection.py
+++ lane_detection.py
@@ -9,21 +9,23 @@ from tqdm.notebook import tqdm_notebook
 
 
 def divide_video_into_frames():
-    capturedVideo = cv2.VideoCapture('video-trim.mp4')
+    capturedVideo = cv2.VideoCapture('video-trim-longer.mp4')
     count = 0
-    while capturedVideo.isOpened() and count <= 500:
+    while capturedVideo.isOpened() and count <= 5000:
         success, frame = capturedVideo.read()
         if success:
-            frame_scaled = rescaleFrame(frame, 0.5)
+            frame_scaled = rescale_cnvrt_Frame(frame, 0.5)
             cv2.imwrite("frames/frame%d.png" % count, frame_scaled)
         count += 1
 
 
-def rescaleFrame(frame, scale=0.75):
+def rescale_cnvrt_Frame(frame, scale=0.75):
     height = int(frame.shape[0] * scale)
     width = int(frame.shape[1] * scale)
     dimensions = (width, height)
-    return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
+    img = cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
+    gry_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return gry_img
 
 
 def load_sort_frames():
@@ -32,35 +34,38 @@ def load_sort_frames():
     col_images = []
     for i in tqdm_notebook(col_frames):
         img = cv2.imread('frames/' + i, IMREAD_GRAYSCALE)
-        # uncomment below lines to see the frame images in gray scale
-        # cv2.imshow("display", img)
-        # cv2.waitKey(0)
         col_images.append(img)
 
     return col_images
 
 
 def masking_lane_detection_writing(col_images):
-    stencil = np.zeros_like(col_images[0][:, :])
+    mask = np.zeros(col_images[0].shape[:2], dtype='uint8')
     # specify coordinates of the polygon
-    polygon = np.array([[200, 400], [300, 225], [500, 225], [550, 400]])
+    polygon = np.array([[100, 400], [300, 225], [500, 225], [700, 400]])
     # fill polygon with ones
-    cv2.fillConvexPoly(stencil, polygon, 1)
-    # uncomment to show the polygon
-    # plt.figure(figsize=(10, 10))
-    # plt.imshow(stencil, cmap="gray")
-    # plt.show()
+    cv2.fillConvexPoly(mask, polygon, 1)
     cnt = 0
     for img in tqdm_notebook(col_images):
-        masked = cv2.bitwise_and(img[:, :], img[:, :], mask=stencil)
-        ret, thresh = cv2.threshold(masked, 130, 145, cv2.THRESH_BINARY)
+        masked = cv2.bitwise_and(img, img, mask=mask)
+        ret, thresh = cv2.threshold(masked, 130, 255, cv2.THRESH_BINARY)
+        # cv2.imshow("Threshold", thresh)
+        # cv2.waitKey(0)
         lines = cv2.HoughLinesP(thresh, 1, np.pi / 180, 30, maxLineGap=200)
         dmy = img.copy()
         try:
             for line in lines:
                 x1, y1, x2, y2 = line[0]
+                slope = (y2 - y1) / (x2 - x1)
+                if slope > 0.4:
+                    print("right lane")
+                elif slope < -0.4:
+                    print("left lane")
                 cv2.line(dmy, (x1, y1), (x2, y2), (255, 0, 0), 3)
+                # cv2.imshow("img", dmy)
+                # cv2.waitKey(0)
             cv2.imwrite('detected/' + str(cnt) + '.png', dmy)
+
         except TypeError:
             cv2.imwrite('detected/' + str(cnt) + '.png', img)
         cnt += 1
@@ -90,7 +95,7 @@ def video_from_detected():
 
 
 if __name__ == "__main__":
-    divide_video_into_frames()
+    # divide_video_into_frames()
     col_images = load_sort_frames()
     masking_lane_detection_writing(col_images)
     video_from_detected()
